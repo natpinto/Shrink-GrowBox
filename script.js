4e1bbdd9cda3dbc87c8857af0fc891c5f1341849
natpinto <natalie.vp06@gmail.com>
2021-01-30 19:39:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var box = document.getElementById("box");
+
+function shrink() {
+  box.style.width = "30px";
+  box.style.height = "30px";
+}
+
+function grow() {
+  box.style.width = "200px";
+  box.style.height = "200px";
+}
